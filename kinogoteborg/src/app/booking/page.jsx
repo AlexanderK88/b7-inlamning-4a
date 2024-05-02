2ececb0947d042dd8ee71b954aec4dbc5d0f51c9
Mats Johansson <matjoh50@gmail.com>
2024-05-02 12:05:31 +0200

    minor changes for visual understanding

--- kinogoteborg/src/app/booking/page.jsx
+++ kinogoteborg/src/app/booking/page.jsx
@@ -32,25 +32,37 @@ export default function Page() {
   return (
     <div className="flex flex-col border h-screen m-0 w-[80vw] m-auto">
       <div className="grid md:grid-cols-4 md:grid-rows-8 gap-4">
-        <div className="md:row-span-6 md:col-start-4 md:row-start-1 border">movie data</div>
+        <div className="md:row-span-6 md:col-start-4 md:row-start-1 border h-[70vh]">
+          movie data
+          <div className="bg-gray-600 w-[90%] h-[50%] flex justify-center m-auto my-2">
+            movie poster
+          </div>
+          <div className="bg-gray-600 w-[90%] h-[10%] flex justify-center m-auto my-2">
+            movie details
+          </div>
+          <div className="bg-gray-600 w-[90%] h-[10%] flex justify-center m-auto my-2">
+            some more details
+          </div>
+        </div>
 
         <div className="md:col-span-3 flex flex-row align-center justify-evenly border p-2 ">
           <p>screening date</p>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
         </div>
 
         <div className="md:col-span-2 md:col-start-1 md:row-start-2 flex flex-row align-center justify-evenly p-2 border">
           <p>screening times</p>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
-          <div className="bg-gray-200 w-[8vw] h-[4vw]"></div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
+          <div className="bg-gray-200 w-[8vw] h-[4vw]">content</div>
         </div>
         <div className="md:col-start-3 md:row-start-2 border">amount of guests</div>
-        <div className="md:col-span-3 md:row-span-6 md:col-start-1 md:row-start-3 border">
+        <div className="grid md:col-span-3 md:row-span-6 md:col-start-1 md:row-start-3 border min-h-[30vh] ">
           saloon render
+          <div className="h-2 w-[80%] bg-black m-auto rounded-md">content</div>
         </div>
         <div className="md:col-start-4 md:row-start-8 border justify-center items-center grid">
           <Button onClick={() => setBookNow(true)} className={"w-[10em]"}>
