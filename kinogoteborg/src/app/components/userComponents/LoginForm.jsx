e972a0e48c20ca39762261f6593b9e7cca47e967
Kookiie88 <alexander@jj-media.se>
2024-05-03 16:43:29 +0700

    Created POST route to register user
    Co-authored-by: Weeezzt <Weeezzt@users.noreply.github.com>

--- kinogoteborg/src/app/components/userComponents/LoginForm.jsx
+++ kinogoteborg/src/app/components/userComponents/LoginForm.jsx
@@ -14,10 +14,7 @@ export default function LoginForm({ setIsRegistered }) {
           Password
         </label>
         <input type="password" name="password" placeholder="password" />
-        <button
-          className=" mt-4 bg-red-800 hover:bg-red-700 text-stone-300 font-bold cursor-pointer px-6 py-2 rounded-md"
-          onClick=""
-        >
+        <button className=" mt-4 bg-red-800 hover:bg-red-700 text-stone-300 font-bold cursor-pointer px-6 py-2 rounded-md">
           Login
         </button>
         <div>
