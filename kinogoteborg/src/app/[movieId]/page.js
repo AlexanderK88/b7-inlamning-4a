82df49c18bed5cf2bd5c2b1d45a28816bd6a68e2
Mats Johansson <matjoh50@gmail.com>
2024-05-06 21:47:19 +0200

    clean up from mixed branches and 'dead' code

--- kinogoteborg/src/app/[movieId]/page.js
+++
@@ -1,6 +0,0 @@
-//import rendersaloon to render page.
-//build modal to use booking.js 
-//add state for booking to use, might be better placed in booking.js
-renderSaloon(2)
-  .then(cinema => console.log(cinema))
-  .catch(error => console.error(error));
