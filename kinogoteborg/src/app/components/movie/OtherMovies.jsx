da11d77ee66ad664b7699b773032fe79bfcc7a5f
Weeezzt <william.vesterberg@lerniastudent.se>
2024-05-08 10:58:55 +0200

    Just made a simple component for otherMovies

---
+++ kinogoteborg/src/app/components/movie/OtherMovies.jsx
@@ -0,0 +1,8 @@
+export default async function OtherMovies() {
+  return (
+    <div className="flex flex-col sm:flex-row ">
+      <img src="" alt="" className="rounded-md" />
+      <img src="" alt="" className="rounded-md" />
+    </div>
+  );
+}
--- kinogoteborg/src/app/movies/[movieID]/page.jsx
+++ kinogoteborg/src/app/movies/[movieID]/page.jsx
@@ -1,9 +1,11 @@
 import Movie from "@/app/components/movie/Movie";
+import OtherMovies from "@/app/components/movie/OtherMovies";
 
 export default function page() {
   return (
     <div>
       <Movie />
+      <OtherMovies />
     </div>
   );
 }
