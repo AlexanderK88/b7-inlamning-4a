24ff7fae39833c3baf0ecae82a72017dd7b7e450
Kookiie88 <alexander@jj-media.se>
2024-05-12 21:06:17 +0700

    Created the route for posting movies
    Co-authored-by: Weeezzt <Weeezzt@users.noreply.github.com>

--- kinogoteborg/src/app/api/movies/route.js
+++ kinogoteborg/src/app/api/movies/route.js
@@ -13,3 +13,34 @@ export async function GET() {
     return NextResponse.json({ success: "false", error: err });
   }
 }
+
+export async function POST(req) {
+  try {
+    const { title, description, imdbRating, url, alt, releaseDate, genre } = await req.json();
+
+    await connectToDb();
+
+    const movies = await Movie.find({});
+
+    const length = movies.length;
+
+    await Movie.create({
+      id: length + 1,
+      attributes: {
+        title,
+        description,
+        imdbRating,
+        image: {
+          url,
+          alt,
+        },
+        releaseDate,
+        genre,
+      },
+    });
+
+    return NextResponse.json({ message: "Movie registered successfully", status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error while registering movie", status: 400 });
+  }
+}
