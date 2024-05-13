3b05bc7c6c227aca3a65351e523dedaf2ca8910d
Kookiie88 <alexander@jj-media.se>
2024-05-14 02:15:47 +0700

    removed old console.logs
    Co-authored-by: Weeezzt <Weeezzt@users.noreply.github.com>

--- kinogoteborg/src/app/api/auth/[...nextauth]/route.js
+++ kinogoteborg/src/app/api/auth/[...nextauth]/route.js
@@ -38,9 +38,7 @@ export const authOptions = {
   },
   callbacks: {
     async jwt({ token, user, session }) {
-      console.log("jwt callback", { token, user, session });
       if (user) {
-        //pass user id and number to token
         return {
           ...token,
           phoneNumber: user.phoneNumber,
@@ -52,8 +50,6 @@ export const authOptions = {
       return token;
     },
     async session({ token, user, session }) {
-      console.log("session callback", { token, user, session });
-      //Pass user id and number to session
       return {
         ...session,
         user: {
