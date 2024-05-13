d4c7546248fda8909755f58f0b4af44e799396f3
Mats Johansson <matjoh50@gmail.com>
2024-05-13 14:58:40 +0200

    removal of file

--- kinogoteborg/src/app/components/booking/Saloon.jsx
+++
@@ -1,9 +0,0 @@
-import { fetchSaloon } from "@/scripts/fetchSaloonLayout";
-import { RenderSaloon } from "./RenderSaloon";
-
-export default async function Saloon({ saloonNumber, seats }) {
-  console.log("saloon render");
-  const data = await fetchSaloon(saloonNumber);
-
-  return <RenderSaloon data={data} saloonNumber={saloonNumber} seats={seats} />;
-}
