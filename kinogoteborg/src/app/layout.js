6ee06c3bf06aa2a7dd078fa03315eec46e878d1f
Kookiie88 <alexander@jj-media.se>
2024-05-15 17:35:37 +0700

    removed a console.log and alinebreak
    Co-authored-by: Weeezzt <Weeezzt@users.noreply.github.com>

--- kinogoteborg/src/app/layout.js
+++ kinogoteborg/src/app/layout.js
@@ -18,7 +18,6 @@ export default function RootLayout({ children }) {
         <body className={`${inter.className} bg-stone-700`}>
           <Header />
           <main className="min-h-screen">{children}</main>
-
           <Footer />
         </body>
       </AuthProvider>
