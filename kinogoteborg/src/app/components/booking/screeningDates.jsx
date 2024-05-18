5f7dc5b86a51b663922855d8c5710e33052ef3d3
AnnaLenaO <annalenao@me.com>
2024-05-18 12:35:29 +0200

    responsive red boxes for date & time

--- kinogoteborg/src/app/components/booking/screeningDates.jsx
+++ kinogoteborg/src/app/components/booking/screeningDates.jsx
@@ -48,7 +48,7 @@ export default function ScreeningDates({ id, setSelectedDate, selectedDate }) {
         <div
           key={index}
           onClick={() => handleDateClick(date)}
-          className={`flex items-center justify-center w-[8vw] h-[4vw] p-2 rounded text-center hover:bg-red-700 text-stone-300 font-bold cursor-pointer 
+          className={`flex items-center justify-center min-w-[8vw] min-h-[4vw] p-2 rounded text-center hover:bg-red-700 text-stone-300 font-bold cursor-pointer 
                     ${date === selectedDate ? "bg-red-900 text-white font-bold" : "bg-gray-500 text-white"} mb-2 md:mb-0 mx-1`}
         >
           {date}
--- kinogoteborg/src/app/components/booking/screeningTimes.jsx
+++ kinogoteborg/src/app/components/booking/screeningTimes.jsx
@@ -44,7 +44,7 @@ export default function ScreeningTimes({ id, setSelectedTime, selectedTime, sele
         <div
           key={index}
           onClick={() => handleTimeClick(time)}
-          className={`flex items-center justify-center w-[8vw] h-[4vw] p-2 rounded text-center hover:bg-red-700 text-stone-300 font-bold cursor-pointer 
+          className={`flex items-center justify-center min-w-[8vw] min-h-[4vw] p-2 rounded text-center hover:bg-red-700 text-stone-300 font-bold cursor-pointer 
                     ${time === selectedTime ? "bg-red-900 text-white font-bold" : "bg-gray-500 text-white"} mb-2 md:mb-0 mx-1`}
         >
           {time}
