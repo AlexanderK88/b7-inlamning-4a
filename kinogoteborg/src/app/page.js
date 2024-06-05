291ff5df4a6164d55e6f5ecdbc913f69f7651bc3
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-06-05 14:38:47 +0200

    1. Added components for firstpage, including hero image, some text and a component for rendering some random movies. 2. Skeleton/Loading-component is added and shows meanwhile data is fetching.

--- kinogoteborg/src/app/page.js
+++ kinogoteborg/src/app/page.js
@@ -1,13 +1,27 @@
-import Header from "./components/headerFooter/header";
-import Footer from "./components/headerFooter/footer";
+import { Suspense } from "react";
+import RandomMovies from "./components/heroSection/RandomMovies";
+import Hero from "./components/heroSection/Hero";
+import CardSkeleton from "./ui/Skeletons";
 
 export default function Home() {
   return (
     <>
-      {/* <Header /> */}
-      <h1 className="grid place-items-center mt-20">hejsan hejsan</h1>
-      <h1 className="flex justify-center">detta är andra raden</h1>
-      {/* <Footer /> */}
+      <Hero />
+      <div className="">
+        <h2 className="flex flex-col text-white font-bold text-7xl items-center mb-30">
+          With a wide variety of movies for every taste!
+        </h2>
+      </div>
+      <h1 className="grid place-items-center mt-20 text-white font-bold">
+        Some of the movies we have right now
+      </h1>
+      <Suspense
+        fallback={
+          <p className="text-center text-white">Loading some random movies</p>
+        }
+      >
+        <RandomMovies />
+      </Suspense>
     </>
   );
 }
